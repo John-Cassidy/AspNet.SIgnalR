5850feb22952a5f8e74e6703020f9ea2793e5f49
John Cassidy <John.Cassidy_1@philips.com>
2022-10-11 13:25:52 -0400

    add OrderCoffee API Route to CoffeeController

--- AspNet.SignalR/Controllers/CoffeeController.cs
+++ AspNet.SignalR/Controllers/CoffeeController.cs
@@ -1,10 +1,23 @@
 ﻿using System.Web.Http;
+using AspNet.SignalR.Models;
 
 namespace AspNet.SignalR.Controllers
 {
 	[RoutePrefix("api/coffee")]
 	public class CoffeeController : ApiController
 	{
+
+		private static int OrderId;
+
+		[HttpPost]
+		public int OrderCoffee(Order order)
+		{
+			//var hubContext = GlobalHost.ConnectionManager.GetHubContext<CoffeeHub>();
+			//hubContext.Clients.All.NewOrder(order);
+			OrderId++;
+			return OrderId;
+		}
+
 		[HttpGet]
 		[Route("")]
 		public IHttpActionResult GetStatus()
