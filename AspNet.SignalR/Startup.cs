c3af91c0552b5d0f9e6d3466b76a8a4fd8e6557f
John Cassidy <John.Cassidy_1@philips.com>
2022-10-11 13:23:51 -0400

    add Startup.cs that maps singalR() when app starts

---
+++ AspNet.SignalR/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(AspNet.SignalR.Startup))]
+
+namespace AspNet.SignalR
+{
+	public class Startup
+	{
+		public void Configuration(IAppBuilder app)
+		{
+			app.MapSignalR();
+		}
+	}
+}
