a145011877d94fe9d288e863dffd85178cdf25a0
John Cassidy <John.Cassidy_1@philips.com>
2022-10-11 11:40:51 -0400

    initialize project

---
+++ AspNet.SignalR/Controllers/CoffeeController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+
+namespace AspNet.SignalR.Controllers
+{
+	[RoutePrefix("api/coffee")]
+	public class CoffeeController : ApiController
+	{
+		[HttpGet]
+		[Route("")]
+		public IHttpActionResult GetStatus()
+		{
+			return Ok();
+		}
+
+	}
+}
